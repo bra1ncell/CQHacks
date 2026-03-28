8a51a070618fc49b614de5bec04c7fc51da92900
bra1n <lsanchelli9@gmail.com>
2026-03-28 12:01:51 -0500

    correction

---
+++ src/main/java/Project/Race.java
@@ -0,0 +1,25 @@
+package Project;
+
+public class Race {
+    /*
+     * Time,Driver,DriverNumber,LapTime,LapNumber,Stint,PitOutTime,PitInTime,Sector1Time,Sector2Time,Sector3Time,Sector1SessionTime,Sector2SessionTime,
+     * Sector3SessionTime,SpeedI1,SpeedI2,SpeedFL,SpeedST,IsPersonalBest,Compound,TyreLife,FreshTyre,Team,LapStartTime,LapStartDate,TrackStatus,Position,
+     * Deleted,DeletedReason,FastF1Generated,IsAccurate,Round,Country,Location,Event Name
+     * */
+
+    String country;
+    int rounds;
+    String location;
+    String eventName;
+
+    ArrayList<>
+
+    public Race(int rounds, String country, String location, String eventName) {
+        this.rounds = rounds;
+        this.country = country;
+        this.location = location;
+        this.eventName = eventName;
+    }
+
+
+}
